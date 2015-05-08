28088b18f920319394d7c524d86be72aa7cff514
Viktor Benei <viktor.benei@gmail.com>
2015-05-08 11:21:35 +0200

    fewer snapshots, for testing

--- bitrise_fastlane_test.sh
+++ bitrise_fastlane_test.sh
@@ -5,14 +5,29 @@ set -e
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+#
+# --- Setup
+STARTTIME=$(date +%s)
+
 gem install fastlane
 
 # ImageMagick for FrameIt
 # brew update && brew install imagemagick
 
+ENDTIME=$(date +%s)
+echo
+echo " (i) Setup took $(($ENDTIME - $STARTTIME)) seconds to complete"
+echo
+
+
+#
+# --- Fastlane
+
 fastlane test_and_snapshot
 
-# move screenshots to the Deploy folder
+
+#
+# --- Move screenshots to the Deploy folder
 rm -rf "${BITRISE_DEPLOY_DIR}/screenshots"
 mkdir -p "${BITRISE_DEPLOY_DIR}/screenshots"
 mv "${THIS_SCRIPT_DIR}/fastlane/screenshots" "${BITRISE_DEPLOY_DIR}/screenshots"
